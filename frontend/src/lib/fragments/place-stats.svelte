1d8adc72a2f2518aa216b4f6f7ee2b49c798c275
Kevin Daněk <heindrich73@gmail.com>
2025-04-09 17:18:07 +0200

    feat: Loading placeholder

--- frontend/src/lib/fragments/place-stats.svelte
+++ frontend/src/lib/fragments/place-stats.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/card';
-	import { Skeleton } from '$lib/components/ui/skeleton';
     import { onMount, type ComponentType } from 'svelte';
+    import { Skeleton } from '$lib/components/ui/skeleton';
 	import {
 		Globe,
 		Waves,
@@ -21,15 +21,14 @@
     }
 
     let isLoading = $state<boolean>(true);
-    let stats = $state<Statistic[]>([]);
+    let stats = $state<Statistic[]>(new Array(8).fill({name: "", value: "", description: "", Icon: Globe}));
 
     async function getData() {
 		// We bangin'
-		setTimeout(() => {}, 1000);
-
+		await new Promise((resolve) => setTimeout(resolve, 5000))
         const numberFormatter = new Intl.NumberFormat('cs-CZ', { style: 'decimal' });
-
-        stats.push(
+        
+        stats = [
             {
                 name: "Atmosférický tlak",
                 value: `${numberFormatter.format(1021)} hPa`,
@@ -78,31 +77,7 @@
                 description: new Date().toLocaleString(),
                 Icon: ThermometerSun
             }
-        );
-
-		/*pressure.atmosphere.value = `${numberFormatter.format(1021)} hPa`;
-		pressure.atmosphere.date = new Date().toLocaleString();
-
-		pressure.sea.value = `${numberFormatter.format(1021)} hPa`;
-		pressure.sea.date = new Date().toLocaleString();
-
-		pressure.ground.value = `${numberFormatter.format(991)} hPa`;
-		pressure.ground.date = new Date().toLocaleString();
-
-        humidity.value = `${Number(60 / 100).toLocaleString("cs-CZ", { style: "percent" })}`;
-        humidity.date = new Date().toLocaleString();
-
-		wind.speed.value = `${numberFormatter.format(69)} m/s`;
-		wind.speed.date = new Date().toLocaleString();
-
-        wind.degree.value = `${numberFormatter.format(121)} °`;
-		wind.degree.date = new Date().toLocaleString();
-
-		temperature.max.value = `${numberFormatter.format(69)} °C`;
-		temperature.max.date = new Date().toLocaleString();
-
-		temperature.min.value = `${numberFormatter.format(10)} °C`;
-		temperature.min.date = new Date().toLocaleString();*/
+        ];
 
 		isLoading = false;
 	}
@@ -114,15 +89,28 @@
 
 <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
     {#each stats as { name, value, description, Icon }}
-        <Card.Root>
-            <Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
-                <Card.Title class="text-sm font-medium">{name}</Card.Title>
-                <Icon class="text-muted-foreground h-4 w-4"/>
-            </Card.Header>
-            <Card.Content>
-                <div class="text-2xl font-bold">{value}</div>
-                <p class="text-muted-foreground text-xs">{description}</p>
-            </Card.Content>
-        </Card.Root>
+        {#if isLoading}
+            <Card.Root>
+                <Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <Card.Title><Skeleton class="w-32 h-3.5"/></Card.Title>
+                    <Skeleton class="w-4 h-4"/>
+                </Card.Header>
+                <Card.Content class="flex flex-col gap-1">
+                    <Skeleton class="w-64 h-8"/>
+                    <Skeleton class="w-72 h-4"/>
+                </Card.Content>
+            </Card.Root>
+        {:else}
+            <Card.Root>
+                <Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <Card.Title class="text-sm font-medium">{name}</Card.Title>
+                    <Icon class="text-muted-foreground h-4 w-4"/>
+                </Card.Header>
+                <Card.Content>
+                    <div class="text-2xl font-bold">{value}</div>
+                    <p class="text-muted-foreground text-xs">{description}</p>
+                </Card.Content>
+            </Card.Root>
+        {/if}
     {/each}
 </div>
