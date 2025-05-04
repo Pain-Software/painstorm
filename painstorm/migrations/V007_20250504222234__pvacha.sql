3c7562f4a0ce82f33e7619c806fde652a4c3d2d1
Pavel Vácha <pavel.vachaaa@gmail.com>
2025-05-04 23:37:03 +0200

    feat: pq retention, frontend docker env

--- painstorm/migrations/V007_20250504222234__pvacha.sql
+++ painstorm/migrations/V007_20250504222234__pvacha.sql
@@ -2,3 +2,11 @@ COPY city(name, findname, country, longitude, latitude)
 FROM '/data/migrations/cities.csv'
 DELIMITER ','
 CSV HEADER;
+
+CREATE EXTENSION pg_cron;
+
+SELECT cron.schedule(
+  'delete_old_measurements',
+  '*/30 * * * *', 
+  $$DELETE FROM measurement WHERE created_at < NOW() - INTERVAL '2 days'$$
+);
